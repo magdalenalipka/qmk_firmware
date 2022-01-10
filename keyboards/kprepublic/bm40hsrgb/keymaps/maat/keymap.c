b80ee8acf30f164a7ce7f8cf6522272a3292f115
Magda Lipka <dev@magdalipka.com>
2022-01-10 15:12:44 +0100

    add maat keymap and capslock indicator

---
+++ keyboards/kprepublic/bm40hsrgb/keymaps/maat/keymap.c
@@ -0,0 +1,90 @@
+/* Copyright 2020 tominabox1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layers {
+  _QWERTY,
+  _SPECIAL,
+  _NUMBERS,
+  _FUNCTION,
+  _GAME_QWERTY,
+  _GAME_NUMBERS,
+  _GAME_ARROWS
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_QWERTY] = LAYOUT_planck_mit(
+        KC_ESC,    KC_Q,    KC_W,          KC_E,    KC_R,          KC_T,    KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LSFT,  KC_A,    KC_S,          KC_D,    KC_F,          KC_G,    KC_H,    KC_J,         KC_K,    KC_L,    KC_BSLS, KC_ENT,
+        KC_LCTL,  KC_Z,    KC_X,          KC_C,    KC_V,          KC_B,    KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RALT ,
+        KC_TAB,   KC_LALT, KC_CAPS_LOCK,  KC_LGUI, MO(_SPECIAL),     KC_SPC,        MO(_NUMBERS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+    
+    [_SPECIAL] = LAYOUT_planck_mit(
+        KC_TILD, KC_EXLM, KC_AT,     KC_HASH,   XXXXXXX,    XXXXXXX,   XXXXXXX,   KC_LPRN,       KC_RPRN, KC_LCBR, KC_RCBR, KC_PLUS,
+        _______, KC_DLR,  KC_PERC,   KC_CIRC,   XXXXXXX,    XXXXXXX,   XXXXXXX,   KC_LBRC,       KC_RBRC, KC_QUOT, KC_DQUO, KC_MINS,
+        _______, KC_AMPR, KC_ASTR,   KC_GRV,    XXXXXXX,    XXXXXXX,   XXXXXXX,   KC_SCLN,       KC_COLN, KC_UNDS, KC_EQL,  KC_INS,
+        _______, _______, XXXXXXX,   XXXXXXX,   _______,         _______,         MO(_FUNCTION), KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    ),
+    
+    [_NUMBERS] = LAYOUT_planck_mit(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_1,    KC_2, KC_3,    KC_DEL,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_4,    KC_5, KC_6,    KC_PSCR,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_7,    KC_8, KC_9,    XXXXXXX,
+        _______, _______, XXXXXXX, _______, MO(_FUNCTION),     _______,      _______, XXXXXXX, KC_0, XXXXXXX, XXXXXXX
+    ),
+    
+    [_FUNCTION] = LAYOUT_planck_mit(
+        RESET,   XXXXXXX, RGB_MOD, RGB_HUD, RGB_HUI, KC_BRID,    KC_BRIU, XXXXXXX, KC_F1,   KC_F2,  KC_F3,  XXXXXXX,
+        DEBUG,   BL_TOGG, BL_STEP, RGB_VAD, RGB_VAI, KC_VOLD,    KC_VOLU, XXXXXXX, KC_F4,   KC_F5,  KC_F6,  XXXXXXX,
+        XXXXXXX, BL_DEC,  BL_INC,  RGB_SAD, RGB_SAI, XXXXXXX,    XXXXXXX, XXXXXXX, KC_F7,   KC_F8,  KC_F9,  XXXXXXX,
+        XXXXXXX, RGB_SPD, RGB_SPI, XXXXXXX, XXXXXXX,        RGB_TOG,      XXXXXXX, KC_F10,  KC_F11, KC_F12, TO(_GAME_QWERTY)
+    ),
+    
+    [_GAME_QWERTY] = LAYOUT_planck_mit(
+        KC_ESC,   KC_Q,    KC_W,          KC_E,              KC_R,              KC_T,    KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LSFT,  KC_A,    KC_S,          KC_D,              KC_F,              KC_G,    KC_H,    KC_J,         KC_K,    KC_L,    KC_BSLS, KC_ENT,
+        KC_LCTL,  KC_Z,    KC_X,          KC_C,              KC_V,              KC_B,    KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_RALT ,
+        KC_TAB,   KC_LALT, KC_CAPS_LOCK,  MO(_GAME_ARROWS),  MO(_GAME_NUMBERS),      KC_SPC,       KC_LGUI,      XXXXXXX, XXXXXXX, XXXXXXX, TG(_GAME_QWERTY)
+    ),
+    
+    [_GAME_NUMBERS] = LAYOUT_planck_mit(
+        _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, KC_0,    XXXXXXX, _______,     _______,      _______, _______, _______, _______, XXXXXXX
+    ),
+    
+    [_GAME_ARROWS] = LAYOUT_planck_mit(
+        _______, XXXXXXX, KC_UP,   XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, XXXXXXX, _______, XXXXXXX,     _______,      _______, _______, _______, _______, XXXXXXX
+    )
+
+};
+
+#ifdef RGB_MATRIX_ENABLE
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t space_index = 41;
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(space_index, RGB_WHITE);
+    }
+}
+#endif
+
---
+++ keyboards/kprepublic/bm40hsrgb/keymaps/maat/readme.md
@@ -0,0 +1,59 @@
+# Maat
+
+## To do
+- [x] custom keymap
+- [x] game layers
+- [x] caps lock indicator
+- [] layers indicators
+
+## Layers
+
+### Qwerty
+| Esc   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+| LShift|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+| LCtrl |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RAlt  |
+| Tab   | LAlt | Caps | GUI  |_SPEC |    Space   || _NUM | Left | Down |  Up  |Right |
+
+### Special
+|   ~  |   !  |   @  |   #  |  xxx |  xxx |  xxx |   (  |   )  |  {   |   }  |  +   |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+|      |  $   |  %   |  ^   |  xxx |  xxx |  xxx |   [  |   ]  |   '  |   "  |  -   |
+|      |  &   |  *   |  `   |  xxx |  xxx |  xxx |  ;   |   :  |   _  | =    |Insert|
+|      |      | xxx  |      | _FUN |            ||      | Home |PgDown| PgUp | End  |
+
+### Numbers
+|  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |   1  |   2  |   3  | Del  |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+|      |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |   4  |   5  |   6  |PtrScr|
+|      |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |  xxx |   7  |   8  |   9  | xxx  |
+|      |      |  xxx |      | _FUN |            ||      |  xxx |   0  | xxx  | xxx  |
+
+### Function (special + numbers)
+| Reset| xxx    |rgbMode+| Hue-  |Hue+   |Bright- |Bright+| xxx  | F1   |F2    |F3    |  xxx       |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+| Debug|BLToggle|BLCycle |Bright-|Bright+|Vol+    |Vol+   | xxx  | F4   | F5   |F6    | xxx        |
+|  xxx |Bl-     |BL+     |Sat-   |Sat +  | xxx    | xxx   |xxx   | F7   |F8    |F9    | xxx        |
+| xxx  |Effect- |Effect+ | xxx   | xxx   |   rbgToggle   || xxx  |F10   | F11  |F12   |_GAME_QWERTY|
+
+###  Game Qwerty
+| Esc   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+| LShift|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+| LCtrl |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RAlt  |
+| Tab   | LAlt | Caps |_G_NUM|_G_ARR|    Space   ||      |      |      |      |Right |
+
+### Game Numbers
+|       |   1  |   2  |   3   |      |      |      |      |      |      |      |      |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+|       |   4  |   5  |   6  |      |      |      |      |      |      |      |      |
+|       |   7  |   8  |   9  |      |      |      |      |      |      |      |      |
+|       |      |   0  | xxx  |      |            ||      |      |      |      |  xxx |
+
+### Game Arrows
+|       |  xxx |  Up  |  xxx |      |      |      |      |      |      |      |      |
+|-|-|-|-|-|-|-|-|-|-|-|-|
+|       | Left | Down | Right|      |      |      |      |      |      |      |      |
+|       |  xxx |  xxx |  xxx |      |      |      |      |      |      |      |      |
+|       |      |  xxx |      |  xxx |            ||      |      |      |      |  xxx |
+
